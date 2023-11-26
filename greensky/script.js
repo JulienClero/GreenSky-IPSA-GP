ff3cdddadf90eaa538fc2f1f20597fbfca72d505
J-Clr <julien.clero@ipsa.fr>
2023-11-26 12:12:44 +0100

    .

--- greensky/script.js
+++ greensky/script.js
@@ -1,8 +1,8 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function() {
     let currentIndex = 0;
     const slides = document.querySelectorAll('.image-slider img');
+    const slider = document.querySelector('.image-slider');
+
 
     function showSlide(index) {
         slides.forEach((slide, i) => {
@@ -15,26 +15,13 @@ document.addEventListener('DOMContentLoaded', function() {
         showSlide(currentIndex);
     }
 
-    function adjustSliderHeight() {
-        const slider = document.querySelector('.image-slider');
-        const newHeight = window.innerHeight * 0.4; // 40% of viewport height
-        slider.style.maxHeight = `${newHeight}px`;
-
-        // Ajuster la hauteur des images pour correspondre à la nouvelle hauteur du slider
-        slides.forEach((slide) => {
-            slide.style.maxHeight = `${newHeight}px`;
-        });
-    }
-
     // Appel initial pour commencer le défilement immédiatement
     nextSlide();
 
-    setInterval(nextSlide, 2500); // Change slide every 5 seconds
+    setInterval(nextSlide, 2500); // Change slide every 2.5 seconds
 
     // Réagir aux changements de taille de la fenêtre
-    window.addEventListener('resize', function() {
-        adjustSliderHeight();
-    });
+    window.addEventListener('resize', adjustSliderHeight);
 
     // Appel initial pour ajuster la hauteur au chargement de la page
     adjustSliderHeight();
