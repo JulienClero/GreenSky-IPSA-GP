8ef83b0780a4f70b1ea9ce6ca2f3c0d68479c63a
J-Clr <julien.clero@ipsa.fr>
2023-11-27 20:30:16 +0100

    .

--- greensky/script.js
+++ greensky/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentIndex = 0;
     const slides = document.querySelectorAll('.image-slider img');
     const slider = document.querySelector('.image-slider');
-
+    const parallaxElements = document.querySelectorAll('.parallax');
 
     function showSlide(index) {
         slides.forEach((slide, i) => {
@@ -15,6 +15,24 @@ document.addEventListener('DOMContentLoaded', function() {
         showSlide(currentIndex);
     }
 
+    function adjustSliderHeight() {
+        const newHeight = window.innerHeight * 0.4; // 40% of viewport height
+        slider.style.maxHeight = `${newHeight}px`;
+
+        // Ajuster la hauteur des images pour correspondre à la nouvelle hauteur du slider
+        slides.forEach((slide) => {
+            slide.style.maxHeight = `${newHeight}px`;
+        });
+    }
+
+    function handleParallax() {
+        parallaxElements.forEach(function(element) {
+            const speed = element.getAttribute('data-speed');
+            const yPos = window.scrollY * speed;
+            element.style.transform = 'translateY(' + yPos + 'px)';
+        });
+    }
+
     // Appel initial pour commencer le défilement immédiatement
     nextSlide();
 
@@ -25,4 +43,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Appel initial pour ajuster la hauteur au chargement de la page
     adjustSliderHeight();
+
+    // Ajout de l'écouteur d'événements pour l'effet de parallaxe
+    window.addEventListener('scroll', handleParallax);
 });
